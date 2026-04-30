af73a8f29017421b3ea7c630f472e44df772f8c0
zhangshuo <896841013@qq.com>
2026-04-30 13:37:04 +0800

    fix: add missing ContextCompat import

--- app/src/main/java/com/quizhelper/app/FloatingButtonService.kt
+++ app/src/main/java/com/quizhelper/app/FloatingButtonService.kt
@@ -20,6 +20,7 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.chinese.ChineseTextRecognizerOptions
